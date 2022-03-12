54327701355498d540480212a2fad69711009786
hugosenari <hugosenari@gmail.com>
2022-03-12 03:15:19 -0300

    chore: re index mangle

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Yet another iterator library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."mangle-master".url = "path:./master";
+  inputs."mangle-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mangle-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
